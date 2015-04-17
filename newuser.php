78406251b4acc0415f29d46fb1bdb3e540cf6b78
Jeremy <jeremymwatson@gmail.com>
2015-04-17 08:48:16 -0400

    new user reg work

--- newuser.php
+++ newuser.php
@@ -29,6 +29,15 @@ $r5 = $mysqli->real_escape_string($_POST['movie5rating']);
 
 $register_attempt = $mysqli->query("SELECT * FROM `users` WHERE email='$email'");
 
+$movie_attempt = $mysqli->query("SELECT `id` FROM `movies` WHERE `title` LIKE '$m1' OR `title` 
+    LIKE '$m2' OR `title` LIKE '$m3' OR `title` LIKE '$m4' OR `title` LIKE  '$m5' ");
+
+
+while($row = $movie_attempt->fetch_row()) {
+   $id_array[] = $row[0];
+    
+}
+
 if ($register_attempt->num_rows >= 1){
     $_SESSION['email_fail'] = true;
     $failed=true;
@@ -41,10 +50,23 @@ $_SESSION['registration_failed']=false;
 
 $insert = "INSERT  INTO `users`  (`email`, `password`, `age`,`gender`,`occupation`,`zip_code`) 
                 VALUES ('{$email}', '{$password}', '{$age}', '{$gender}', '{$occupation}','{$zip}')";
+
+
 if ($mysqli->query($insert)) {
+
+/*    $uidq = $mysqli->query("SELECT `userid` FROM `users` WHERE email='$email'");
+    $uid = $uidq->fetch_row()
+
+    $
+*/
+
+
+
     $_SESSION['registration_success']=true;
-    header("location:main.php");
-    exit();
+    echo "$m1";
+    echo "$r1";
+/*    header("location:main.php");*/
+/*    exit();*/
 } else {
     echo "<p>MySQL error no {$mysqli->errno} : {$mysqli->error}</p>";
     exit();
