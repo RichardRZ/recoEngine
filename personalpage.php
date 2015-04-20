488f3d0c8e142a9b500ddaf8b7fc5643b1ba9c47
Jeremy <jeremymwatson@gmail.com>
2015-04-20 12:12:11 -0400

    fixing site

--- personalpage.php
+++ personalpage.php
@@ -3,7 +3,7 @@ require_once("./include/membersite_config.php");
 
 if(!$fgmembersite->CheckLogin())
 {
-    $fgmembersite->RedirectToURL("signin.php");
+    $fgmembersite->RedirectToURL("main.php");
     exit;
 }
 ?>
